610ae921afff21427414badeed1b77f98c5a4001
Mike <msfurr94@gmail.com>
2019-11-05 13:11:08 -0500

    update

--- Jupyter Notebooks/Resample.m
+++ Jupyter Notebooks/Resample.m
@@ -1,7 +1,7 @@
 clc, clear
 
-flow = readmatrix('FlowScaled.csv');
-sensor = readmatrix('SensorData.csv');
+flow = readmatrix('FlowData_Nov5.csv');
+sensor = readmatrix('SensorData_Nov5.csv');
 
 %%
 
@@ -15,9 +15,19 @@ sensor_7 = sensor(:, 8);
 sensor_8 = sensor(:, 9);
 
 flow_undersampled = resample(flow, length(sensor_1), length(flow));
-flow_undersampled(:,2) = flow_undersampled(:,2)*-1;
+flow_undersampled(:,1) = flow_undersampled(:,1)*-1;
 
 x = linspace(0, 9989, 9989);
 
+%%
+
 figure
-plot(x(3000:6000), flow_undersampled(3000:6000, 2), x(3000:6000), sensor_1(3000:6000), 'LineWidth', 1.5)
+plot(x(10:6000), flow_undersampled(10:6000, 1), 'LineWidth', 1.5)
+
+figure
+plot(x(10:6000), sensor_1(10:6000), 'LineWidth', 1.5)
+
+%%
+
+csvwrite('flow_training.csv', flow_undersampled);
+
